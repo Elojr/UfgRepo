48b811134b493a7904628db531677a0fbc3c91a4
João Pedro Gama F. Mattos <jopedrogama@gmail.com>
2020-04-05 16:56:18 -0300

    consertando os warnings

--- src/app/models/Professor.js
+++ src/app/models/Professor.js
@@ -1,3 +1,20 @@
 import Sequelize, { Model } from 'sequelize';
 
-class Professors extends Models {}
+class Professors extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        institute: Sequelize.STRING,
+        email: Sequelize.STRING,
+        description: Sequelize.STRING,
+      },
+      {
+        tableName: Professors,
+        sequelize,
+      }
+    );
+  }
+}
+
+export default Professors;
